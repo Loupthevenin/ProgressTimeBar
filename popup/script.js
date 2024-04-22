705bb86b374aca4a307095cb149132688bfb4c50
AGENT MPR 2 <agent.mpr2@gd.local>
2024-04-22 15:37:00 +0200

    Create popup.html

---
+++ popup/script.js
@@ -0,0 +1,47 @@
+chrome.action.onClicked.addListener(() => {
+  chrome.action.openOptionsPage();
+});
+
+let config = {
+  startHour: 8,
+  endHour: 17,
+  morning: 4,
+  afternoon: 3,
+  breakHour: 2,
+  timerStep: 1 * 1000,
+};
+
+function loadValues() {
+  document.getElementById("startHour").value = config.startHour;
+  document.getElementById("endHour").value = config.endHour;
+  document.getElementById("morning").value = config.morning;
+  document.getElementById("afternoon").value = config.afternoon;
+  document.getElementById("breakHour").value = config.breakHour;
+  document.getElementById("timerStep").value = config.timerStep;
+}
+
+function saveValues() {
+  config.startHour = document.getElementById("startHour").value;
+  config.endHour = document.getElementById("endHour").value;
+  config.morning = document.getElementById("morning").value;
+  config.afternoon = document.getElementById("afternoon").value;
+  config.breakHour = document.getElementById("breakHour").value;
+  config.timerStep = document.getElementById("timerStep").value;
+
+  chrome.storage.local.set({ config: config }, function () {
+    console.log("Les valeurs ont été enregistrées avec succès !");
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.storage.local.get("config", function (data) {
+    if (data.config) {
+      config = data.config;
+    }
+    loadValues();
+  });
+});
+
+document.getElementById("saveButton").addEventListener("click", function () {
+  saveValues();
+});
